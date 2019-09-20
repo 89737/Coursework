f7adc9a33dc57907d7502dbeb6936ae0a8cb3e70
89737 <89737@farnborough.ac.uk>
2019-09-20 13:53:34 +0100

    Directories updates as well as SDK

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         openDatabase("Login.db"); // the file i want to open
       //UserFunctions.queryToUpdate()
-        closeDatabase();
+    closeDatabase();
     }
 
     private static void openDatabase(String dbFile){
@@ -30,67 +30,12 @@ public class Main {
     }
     private static void closeDatabase(){
         try{
-            // those two lines of code close the database and then displaysa  message that it has disconnected
+            // those two lines of code close the database and then displays a  message that it has disconnected
             db.close();
             System.out.println("disconnected from database.");
 
         }catch (Exception exception) {
             System.out.println("Database disconnection error: " + exception.getMessage());
-        }
-    }
-
-
-    public static void deleteUser ( int UserID, String username, String password){
-        try {
-            // SQL statement that removes a specific part from the database where the requirement is met
-            PreparedStatement ps = db.prepareStatement("DELETE FROM User WHERE UserID = 1");
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
-
-        }
-    }
-
-    public static void updateUser ( int UserID, String username, String password){
-        try {
-            // the SQL statement that sets a change to a ID where there is a matching piece of existing data
-            PreparedStatement ps = db.prepareStatement("UPDATE User SET username = bob, password = bob111 WHERE UserID = 1");
-            ps.executeUpdate();
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
-
-        }
-    }
-
-    public static void insertUser ( int UserID, String Username, String Password){
-        try {
-            //the 2 lines create a user with the properties for that table and then updates the table to insert them in
-            PreparedStatement ps = db.prepareStatement("INSERT INTO User (UserID, username, password) VALUES(5,Legend27,WhoAm1?)");
-            ps.executeUpdate();
-            System.out.println("Record added to Users table");
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
-            System.out.println("Error: something gone wrong. please contact the administrator with the error code WC-WA.");
-        }
-    }
-
-
-    public static void ListUsers () {
-        try {
-            //
-            PreparedStatement ps = db.prepareStatement("SELECT UserID, UserName, Password FROM User");
-            ResultSet results = ps.executeQuery();
-            while (results.next()) ;
-            {
-                int UserID = results.getInt(1);
-                String username = results.getString(2);
-                String Password = results.getString(3);
-                System.out.println(UserID + " " + username + " " + Password);
-
-            }
-
-
-        } catch (Exception exception) {
-            System.out.println("Database error: " + exception.getMessage());
         }
     }
 }
--- src/main/java/UserFunctions.java
+++ src/main/java/UserFunctions.java
@@ -3,22 +3,22 @@ import java.sql.ResultSet;
 
 public class UserFunctions {
 
-    public static void insertUser(String Username, String Password) {
+    public static void insertUser(String Username, String Password, String Email) {
         try {
             //the 2 lines create a user with the properties for that table and then updates the table to insert them in
-            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO User (username, password) VALUES(Legend27,WhoAm1?)");
-            ps.executeUpdate();
-            System.out.println("Record added to Users table");
+            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO User (username, password, email) VALUES(Legend27,WhoAm1?, )");
+            ps.executeUpdate(); //executes the query to update the table
+            System.out.println("Record added to Users table"); //confirmation message that the user table has been updated
         } catch (Exception exception) {
-            System.out.println(exception.getMessage());
+            System.out.println(exception.getMessage()); //displays the error message if any errors have occurred
             System.out.println("Error: something gone wrong. please contact the administrator with the error code WC-WA.");
         }
     }
 
-    public static void updateUser(int UserID, String username, String password) {
+    public static void updateUser(int UserID, String username, String password, String Email) {
         try {
             // the SQL statement that sets a change to a ID where there is a matching piece of existing data
-            PreparedStatement ps = Main.db.prepareStatement("UPDATE User SET username = ?, password = ? WHERE UserID = ?");
+            PreparedStatement ps = Main.db.prepareStatement("UPDATE User SET username = ?, password = ?, Email = ? WHERE UserID = ?");
             ps.executeUpdate();
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
@@ -27,7 +27,7 @@ public class UserFunctions {
     }
 
 
-    public static void deleteUser(int UserID, String username, String password) {
+    public static void deleteUser(int UserID, String username, String password, String Email) {
         try {
             // SQL statement that removes a specific part from the database where the requirement is met
             PreparedStatement ps = Main.db.prepareStatement("DELETE FROM User WHERE UserID = ?");
@@ -38,9 +38,9 @@ public class UserFunctions {
     }
 
     public static void ListUsers() {
-        try {
+        try { // try catch method to catch any errors to stop the program breaking
             //
-            PreparedStatement ps = Main.db.prepareStatement("SELECT UserID, UserName, Password FROM User");
+            PreparedStatement ps = Main.db.prepareStatement("SELECT UserID, UserName, Password, Email FROM User");
             ResultSet results = ps.executeQuery();
             //while it is not the end of the database it will display each user with their attributes
             while (results.next()) ;
@@ -49,7 +49,8 @@ public class UserFunctions {
                 int UserID = results.getInt(1);
                 String username = results.getString(2);
                 String Password = results.getString(3);
-                System.out.println(UserID + " " + username + " " + Password);
+
+                System.out.println(UserID + " " + username + " " + Password + " " ); //displays the list of users
 
             }
 
