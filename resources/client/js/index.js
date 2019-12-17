e5d587d3b5d80772544790fdad5315727acc37d7
89737 <89737@farnborough.ac.uk>
2019-12-17 10:30:49 +0000

    things somewhat work

--- resources/client/js/index.js
+++ resources/client/js/index.js
@@ -4,7 +4,7 @@ function pageLoad() {
 
     let myHTML = '<div style="text-align:center;">'   // aligns the text for my main heading
         + '<h1>Flashcard Revision!</h1>' // this is the main title for my index page and will show the writing within the h1 tags
-        + '<img src="/client/img/logo.jpg" align="left" width="300px" height="100" alt="Logo"/>' // this loads in the image i'll be using and has the file path with the width being set to 300px wide
+        + '<img src="/client/img/logo.jpg" align="left" width="300px" height="100px" alt="Logo"/>' // this loads in the image i'll be using and has the file path with the width being set to 300px wide
         + '<div style="font-style: italic;">' //this is for the font style that will be being set for the heading
         + 'Generated at ' + now.toLocaleTimeString() //this displays the time of which the page has been loaded
         + '</div>'
@@ -12,9 +12,62 @@ function pageLoad() {
 
     document.getElementById("testDiv").innerHTML = myHTML; //sets the testDiv to the myHTML variable
 
-    alert("yeet")
+
+
+    const canvas = document.getElementById('chartCanvas');
+    const context = canvas.getContext('2d');
+
+    let myChart = new Chart(context, {
+        type: 'line',   // change this to 'pie' if wanted
+        data: {
+            labels: [
+                'Test 1',
+                'Test 2',
+                'Test 3',
+                'Test 4',
+                'Test 5',
+                'Test 6'
+            ],
+            datasets: [{
+                label: 'Test results',
+                data: [
+                    12,
+                    19,
+                    3,
+                    5,
+                    2,
+                    3
+                ],
+                backgroundColor: [
+                    'red',
+                    'blue',
+                    'yellow',
+                    'green',
+                    'purple',
+                    'orange'
+                ]
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+            responsive: false
+        }
+    });
+
 
 }
 
+function RunLoginpage() {
+
+
+
+    window.location.href = "/client/login.html";
+}
 
 
